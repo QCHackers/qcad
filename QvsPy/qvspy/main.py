ead6c2644d43eecdb52d49c6a6b76534d0029994
cswpy <hncswpy@gmail.com>
2022-04-01 11:48:17 +0400

    Fixed setup

--- QvsPy/qvspy/main.py
+++ QvsPy/qvspy/main.py
@@ -20,24 +20,27 @@ from qiskit.providers.aer.noise import NoiseModel
 
 class QuantumSensor():
 
-    '''
-    __init__() function sets up the noise model and the backend for the quantum simulator. The noise model is a simulator to the quantum computer
-    whereas the backend is the actual quantum sensor. The experiements can be conducted on any one qubit of the quantum circuit. Delays depend
-    on the materials of the sensor.s
-    '''
+    
     def __init__(self) -> None:
+        '''
+        __init__() function sets up the noise model and the backend for the quantum simulator. The noise model is a simulator to the quantum computer
+        whereas the backend is the actual quantum sensor. The experiements can be conducted on any one qubit of the quantum circuit. Delays depend
+        on the materials of the sensor.s
+        '''
         self.noise_model = NoiseModel.from_backend(
                             FakeVigo(), thermal_relaxation=True, gate_error=False, readout_error=False
                             )
         self.backend = AerSimulator.from_backend(FakeVigo(), noise_model=self.noise_model)
         
-    '''
-    @parameter: qubit - the qubit to measure
-    @parameter: delays - the iterator of time intervals to measure
-    @return: the results of T1 experiment of the qubit
-    '''
-    def T1_experiment(self, qubit, delays):
-        exp = T1(qubit=qubit, delays=delays)
+    
+    def T1_experiment(self, t1):
+        '''
+        @parameter: qubit - the qubit to measure
+        @parameter: delays - the iterator of time intervals to measure
+        @return: the results of T1 experiment of the qubit
+        '''
+        delays = np.arange(1e-6, 3 * t1, 2e-5)
+        exp = T1(qubit=0, delays=delays)
         exp.set_transpile_options(scheduling_method='asap')
         exp_data = exp.run(backend=self.backend).block_for_results()
         return exp_data
